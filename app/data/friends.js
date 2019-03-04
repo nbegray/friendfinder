9c3e643b52f8c894a101bba92ff1f9609670c7d1
Natalie Edmonson <natalieedmonson@Natalies-MacBook-Air.local>
2019-03-04 12:26:10 -0700

    updated code

---
+++ app/data/friends.js
@@ -0,0 +1,76 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the reserved tables.
+// Initially we just set it equal to a "dummy" customer.
+// But you could have it be an empty array as well.
+// ===============================================================================
+
+var friends = [
+    {
+      name: "Ahmed",
+      friendphoto: "https://via.placeholder.com/150",
+      friendscore: [1,2,3,4,5,6,7,8,9,1]
+
+      
+    },
+    {
+        name: "Fred",
+        friendphoto: "https://via.placeholder.com/150",
+        friendscore: [1,2,3,4,5,6,7,8,9,1]
+  
+        
+      },
+      {
+        name: "Adam",
+        friendphoto: "https://via.placeholder.com/150",
+        friendscore: [1,2,3,4,5,6,7,8,9,1]
+  
+        
+      },
+      {
+        name: "Isaac",
+        friendphoto: "https://via.placeholder.com/150",
+        friendscore: [1,2,3,4,5,6,7,8,9,1]
+  
+        
+      },
+      {
+        name: "Nicolas",
+        friendphoto: "https://via.placeholder.com/150",
+        friendscore: [1,2,3,4,5,6,7,8,9,1]
+  
+        
+      },
+      {
+        name: "Robert",
+        friendphoto: "https://via.placeholder.com/150",
+        friendscore: [1,2,3,4,5,6,7,8,9,1]
+  
+        
+      },
+      {
+        name: "Daniel",
+        friendphoto: "https://via.placeholder.com/150",
+        friendscore: [1,2,3,4,5,6,7,8,9,1]
+  
+        
+      },
+      {
+        name: "Eric",
+        friendphoto: "https://via.placeholder.com/150",
+        friendscore: [1,2,3,4,5,6,7,8,9,1]
+  
+        
+      },
+      {
+        name: "Wilber",
+        friendphoto: "https://via.placeholder.com/150",
+        friendscore: [1,2,3,4,5,6,7,8,9,1]
+  
+        
+      }
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friends;
+  
